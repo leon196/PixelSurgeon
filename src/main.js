06d9e8de2be391981206474ff3decd51741426b6
Leon <leon.denise@free.fr>
2015-02-05 02:33:12 +0100

    perlinpinpin

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ var bufferCanvas = document.createElement("canvas");
 var bufferContext = bufferCanvas.getContext('2d');
 
 var drawer = new PIXI.Graphics();
-var drawerFrame = { width: Utils.NearestPow2(frame.width / 16), height: Utils.NearestPow2(frame.height / 16) };
+var globalScale = 8;
+var drawerFrame = { width: Utils.NearestPow2(frame.width / globalScale), height: Utils.NearestPow2(frame.height / globalScale) };
 var renderTexture = new PIXI.RenderTexture(drawerFrame.width, drawerFrame.height, null, PIXI.scaleModes.NEAREST);
 var filter = new PIXI.SimpleFilter();
 filter.frame[0] = frame.width;
@@ -73,7 +74,7 @@ function animate()
     	oscillation = Math.cos(tick * 0.001 + i) * 0.5 + 0.5;
 		var frequenceX = 0.001;// + oscillation * 0.02;
 		var frequenceY = 0.2;
-		drawLine(2, Color.Get(tick + i), from, { 
+		drawLine(4, Color.Get(tick + i), from, { 
 			x: (i / count) * drawerFrame.width, 
 			y: drawerFrame.height / 8 + Math.sin((tick + i) * frequenceY) * halfHeight / 4
 		});
@@ -83,10 +84,27 @@ function animate()
 		var frequenceX = 0.001;// + oscillation * 0.02;
 		var frequenceY = 0.2;
 		var sinY = Math.sin((tick + i) * frequenceY);
-		drawLine(2, Color.Gray(sinY * 0.5 + 0.5), from, { 
-			x: (i / count) * drawerFrame.width, 
-			y: drawerFrame.height - drawerFrame.height / 8 + sinY * halfHeight / 4
-		});
+		var x = (i / count) * drawerFrame.width * 3 - drawerFrame.width;
+		var h = halfHeight / 4;
+		var y = drawerFrame.height - h;
+		var perlinScale = 64;
+		var perlin = noise({x: (x + tick * 4) / perlinScale, y: 0, z: 0});
+		drawLine(4, Color.Gray(1 - (perlin * 0.5 + 0.5)), from, { 
+			x: x, y: y - perlin * h });
+	}
+	count *= 2;
+	for (var i = 0; i < count; ++i) {
+    	oscillation = Math.cos(tick * 0.001 + i) * 0.5 + 0.5;
+		var frequenceX = 0.001;// + oscillation * 0.02;
+		var frequenceY = 0.2;
+		var sinY = Math.sin((tick + i) * frequenceY);
+		var x = (i / count) * drawerFrame.width;
+		var h = halfHeight / 2;
+		var perlinScale = 8;
+		var perlin = noise({x: (x + tick * 0.5) / perlinScale, y: 0, z: 0});
+		var perlin2 = noise({x: (x + tick * 1) / 12, y: 0, z: 0});
+		drawLine(4, Color.Gray(1 - (perlin * 0.5 + 0.5)), from, { 
+			x: x + perlin2 * 16, y: halfHeight - perlin * h });
 	}
 
 	renderTexture.render(drawer);
@@ -163,4 +181,32 @@ function getRelativePosition(p)
 function getCroppedPosition(p)
 {
 	return { x: Math.max(0, Math.min(drawerFrame.width, p.x)), y: Math.max(0, Math.min(drawerFrame.height, p.y)) };
+}
+
+function fract(x) { return x % 1; }
+function fractVec3(v) { return { x: v.x % 1, y: v.y % 1 , z: v.z % 1}; }
+function mix(a, b, ratio) { return a * (1 - ratio) + b * ratio };
+
+// hash based 3d value noise
+// function taken from [url]https://www.shadertoy.com/view/XslGRr[/url]
+// Created by inigo quilez - iq/2013
+// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+
+// ported from HLSL to js
+function hash( n ) {
+	return fract(Math.sin(n)*43758.5453);
+}
+ 
+function noise( seed ) {
+    // The noise function returns a value in the range -1.0f -> 1.0f
+    var p = { x: Math.floor(seed.x), y : Math.floor(seed.y), z: Math.floor(seed.z) };
+    var f = fractVec3(seed);
+    f.x = f.x*f.x*(3.0-2.0*f.x);
+    f.y = f.y*f.y*(3.0-2.0*f.y);
+    f.z = f.y*f.y*(3.0-2.0*f.y);
+    var n = p.x + p.y*57.0 + 113.0*p.z;
+    return mix(mix(mix( hash(n+0.0), hash(n+1.0),f.x),
+                   mix( hash(n+57.0), hash(n+58.0),f.x),f.y),
+               mix(mix( hash(n+113.0), hash(n+114.0),f.x),
+                   mix( hash(n+170.0), hash(n+171.0),f.x),f.y),f.z);
 }
